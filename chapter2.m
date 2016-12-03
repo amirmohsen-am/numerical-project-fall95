6b6f68d1b4f2a5512ea24ef7c99e91e8d9c524b9
Amirmohsen Ahanchi <ahanchi@gmail.com>
2016-12-03 23:29:50 +0330

    fix chapter 2 ui error

--- chapter2.m
+++ chapter2.m
@@ -1,26 +1,26 @@
-function varargout = project_guide(varargin)
-% PROJECT_GUIDE MATLAB code for project_guide.fig
-%      PROJECT_GUIDE, by itself, creates a new PROJECT_GUIDE or raises the existing
+function varargout = chapter2(varargin)
+% chapter2 MATLAB code for chapter2.fig
+%      chapter2, by itself, creates a new chapter2 or raises the existing
 %      singleton*.
 %
-%      H = PROJECT_GUIDE returns the handle to a new PROJECT_GUIDE or the handle to
+%      H = chapter2 returns the handle to a new chapter2 or the handle to
 %      the existing singleton*.
 %
-%      PROJECT_GUIDE('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in PROJECT_GUIDE.M with the given input arguments.
+%      chapter2('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in chapter2.M with the given input arguments.
 %
-%      PROJECT_GUIDE('Property','Value',...) creates a new PROJECT_GUIDE or raises the
+%      chapter2('Property','Value',...) creates a new chapter2 or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before project_guide_OpeningFcn gets called.  An
+%      applied to the GUI before chapter2_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to project_guide_OpeningFcn via varargin.
+%      stop.  All inputs are passed to chapter2_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help project_guide
+% Edit the above text to modify the response to help chapter2
 
 % Last Modified by GUIDE v2.5 03-Dec-2016 22:13:45
 
@@ -28,8 +28,8 @@ function varargout = project_guide(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @project_guide_OpeningFcn, ...
-                   'gui_OutputFcn',  @project_guide_OutputFcn, ...
+                   'gui_OpeningFcn', @chapter2_OpeningFcn, ...
+                   'gui_OutputFcn',  @chapter2_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,21 +44,21 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before project_guide is made visible.
-function project_guide_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before chapter2 is made visible.
+function chapter2_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to project_guide (see VARARGIN)
+% varargin   command line arguments to chapter2 (see VARARGIN)
 
-% Choose default command line output for project_guide
+% Choose default command line output for chapter2
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes project_guide wait for user response (see UIRESUME)
+% UIWAIT makes chapter2 wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 % myInit
@@ -75,7 +75,7 @@ t.ColumnEditable = true;
 	
 
 % --- Outputs from this function are returned to the command line.
-function varargout = project_guide_OutputFcn(hObject, eventdata, handles) 
+function varargout = chapter2_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
