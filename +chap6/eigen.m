25d7ea68b9605c317423626761f864603e53a8be
sadra <sadrasafa@gmail.com>
2017-01-05 22:54:56 +0330

    bug fix

--- +chap6/eigen.m
+++ +chap6/eigen.m
@@ -33,7 +33,31 @@ function [ vectors, values] = eigen( A, printLatex)
 %                 vectors(j,i) = t;
 %             end
 %         end
-     
+        B = zeros(n,1);
+%         t = zeros(n-1, n-1);
+        for i = 1:n
+            newA = A - values(i)*eye(n);
+            for j = 1:n
+                B(j) = -newA(j,1);
+            end
+            nums = 1:n;
+            for j = 1:n
+                nums = setdiff(nums, nums(j));
+                newMat = newA(nums, 2:n);
+                if abs(det(newMat)) > 1e-5
+                    break;
+                end
+            end
+            B = setdiff(B, B(j));
+            
+            vectors(:,i) = [1;chap6.cramer(newMat,B)];
+        end
+
+
+            
+            
+                
+       
 
     %printLatex
     z = mat2str(eval(values));
