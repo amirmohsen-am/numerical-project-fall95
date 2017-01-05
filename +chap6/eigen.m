491e5340e5e3a43d302ae1fff2dc723b0347a670
sadra <sadrasafa@gmail.com>
2017-01-05 22:20:48 +0330

    add PrintLatex to function input

--- +chap6/eigen.m
+++ +chap6/eigen.m
@@ -1,4 +1,4 @@
-function [ vectors, values] = eigen( A )
+function [ vectors, values] = eigen( A, printLatex)
         
         n = size(A,1);
         syms v;
@@ -17,24 +17,27 @@ function [ vectors, values] = eigen( A )
         
        % distinctVal = unique(values);
        % s = size(distinctVal,1)
-        for i = 1:n
+%         for i = 1:n
            % for j = 1:n
            %     newA(j,j) = A(j,j) - values(i);
-           % end
-           newA = A - values(i)*eye(n);
-            newA
-            m = vpa(  rref(newA)  ) 
-            vectors(n,i) = 1;
-            for j = n-1:-1:1
-                t = 0;
-                for k = j+1:n
-                    t = vpa(  t - m(j,k) );
-                end
-                vectors(j,i) = t;
-            end
-        end
-        
-                
+%            % end
+%            newA = A - values(i)*eye(n);
+%             newA
+%             m = vpa(  rref(newA)  ) 
+%             vectors(n,i) = 1;
+%             for j = n-1:-1:1
+%                 t = 0;
+%                 for k = j+1:n
+%                     t = vpa(  t - m(j,k) );
+%                 end
+%                 vectors(j,i) = t;
+%             end
+%         end
+     
+
+    %printLatex
+    z = mat2str(eval(values));
+    printLatex(z)
                 
             
             
